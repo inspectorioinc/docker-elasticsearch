2c4dee52776d246b51039d0aa97e125429ffdfe9
Thuan Duong <thuan@inspectorio.com>
2019-11-21 15:51:37 +0700

    Remove -u due to empty remove plugin list

--- setup.bash
+++ setup.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Description: install and remove elasticsearch plugins
 
-set -eoux pipefail
+set -eox pipefail
 
 cmd=/usr/share/elasticsearch/bin/elasticsearch-plugin
 plugins=(repository-s3 repository-gcs https://github.com/vvanholl/elasticsearch-prometheus-exporter/releases/download/6.8.4.0/prometheus-exporter-6.8.4.0.zip)
